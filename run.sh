7fb85edc3ca0b76aa2e24944a3129ab8a0550ed3
NexusLee <tonexus.lee@gmail.com>
2017-08-03 14:23:03 +0800

    modify

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --rm -p 8000:3000 -v ~/work/golang/learn:/go/src/learn -w /go/src/learn tour
+docker run -it --rm -p 8000:3000 -v ~/work/golang/go-learn:/go/src/go-learn -w /go/src/go-learn tour
